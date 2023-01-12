bbeb931f21b6fa54affe78cf9dd1d7c741fb9ae0
venetrius <gergelygjuhasz@gmail.com>
2023-01-12 14:01:43 +0100

    init repo

---
+++ src/test/java/com/sdc/springgroupsiteserver/SpringGroupSiteServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdc.springgroupsiteserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGroupSiteServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
