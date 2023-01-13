71ca014e4b9a83f9afc08dd720410aa6916b303e
venetrius <gergelygjuhasz@gmail.com>
2023-01-13 13:15:56 +0100

    connect to sdc dev db

--- src/main/java/com/sdc/springgroupsiteserver/SpringGroupSiteServerApplication.java
+++ src/main/java/com/sdc/springgroupsiteserver/SpringGroupSiteServerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 @RestController
 public class SpringGroupSiteServerApplication {
 
