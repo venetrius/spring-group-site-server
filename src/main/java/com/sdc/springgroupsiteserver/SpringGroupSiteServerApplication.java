cf7597d64aa0dced4be38a8093cabef0e9b4d330
venetrius <gergelygjuhasz@gmail.com>
2023-03-07 08:36:59 +0100

    add post project to controller

--- src/main/java/com/sdc/springgroupsiteserver/SpringGroupSiteServerApplication.java
+++ src/main/java/com/sdc/springgroupsiteserver/SpringGroupSiteServerApplication.java
@@ -1,11 +1,15 @@
 package com.sdc.springgroupsiteserver;
 
+import com.sdc.springgroupsiteserver.entities.Project;
+import com.sdc.springgroupsiteserver.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 import java.util.Optional;
@@ -25,6 +29,11 @@ public class SpringGroupSiteServerApplication {
         return repo.findAll();
     }
 
+    @PostMapping("/projects")
+    public Project postProject(@RequestBody Project newProject) {
+        return repo.save(newProject);
+    }
+    
     @GetMapping("/projects/{id}")
     public Optional<Project> getProject(@PathVariable Integer id){
         return repo.findById(id);
