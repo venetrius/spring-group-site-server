ae4fdfac4f39a9bcdbbb22ddb9e28df4586efcb5
venetrius <gergelygjuhasz@gmail.com>
2023-03-14 21:05:08 +0100

    save projectId when posting comments

--- src/main/java/com/sdc/springgroupsiteserver/SpringGroupSiteServerApplication.java
+++ src/main/java/com/sdc/springgroupsiteserver/SpringGroupSiteServerApplication.java
@@ -7,11 +7,9 @@ import com.sdc.springgroupsiteserver.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -51,8 +49,16 @@ public class SpringGroupSiteServerApplication {
         return commentRepository.findAll();
     }
 
-    @PostMapping("/projects/{id}/comments")
-    public Comment postProjectComment(@RequestBody Comment newComment) {
+    @PostMapping("/projects/{projectId}/comments")
+    public Comment postProjectComment(@PathVariable Integer projectId, @RequestBody Comment newComment) {
+        Optional<Project> project = repo.findById(projectId);
+        if(project.isEmpty()) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Project not found"
+            );
+        }
+        newComment.setProject(project.get());
+
         return commentRepository.save(newComment);
     }
 }
