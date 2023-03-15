993ae4e57e3abd6605576cfe83e4774b96d40bb1
venetrius <gergelygjuhasz@gmail.com>
2023-03-15 22:39:47 +0100

    add spring-boot-starter-security

---
+++ src/main/java/com/sdc/springgroupsiteserver/security/SecurityConfiguration.java
@@ -0,0 +1,23 @@
+package com.sdc.springgroupsiteserver.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/", "/login").permitAll()
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+
+}
